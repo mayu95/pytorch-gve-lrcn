333eb68c7c929ed71b9a0fefde16a03801397b8e
LI <jialee424@gmail.com>
2020-05-28 12:23:25 +0900

    bilstm

--- eval.sh
+++ eval.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 
+# evaluate cub -vgg16 -epoch 20
+# python main.py --model gve --dataset cub --num-epochs 20 --sc-ckpt ./data/cub/sentence_classifier_ckpt.pth > cub_eval&
+
+
 # evaluate iu origin
-python main.py --model gve --dataset iu --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/origin&
+# python main.py --model gve --dataset iu --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/origin&
 
+# 5.28 00:30 input size = 8192 = cub input size = cub image feature
+# python main.py --model gve --dataset iu --pretrained-model vgg16  --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/input_size8192&
 
+# 5.28 12:15  input8192, bilstm hid*4, sc ep20, gve ep30
+python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/input8192_bi_sc20&
--- pretrain.sh
+++ pretrain.sh
@@ -4,5 +4,10 @@
 # python main.py --model sc --dataset cub > train_cub&
 
 # pretrain iu
-python main.py --model sc --dataset iu > ./log_pretrain/train_iu_orgin&
+# python main.py --model sc --dataset iu > ./log_pretrain/train_iu_orgin&
 
+# 5.28 00:15  input_size = cub input size = cub image feature
+# python main.py --model sc --dataset iu > ./log_pretrain/input_8192&
+
+ # 5.28 11:50  input_8192, bilstm, epoch 20
+python main.py --model sc --dataset iu --num-epochs 20 > ./log_pretrain/input8192_bilstm_ep20&
