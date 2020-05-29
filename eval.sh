b34388d926aa95274a8799f2c41e11d69f28e9a6
LI <jialee424@gmail.com>
2020-05-29 12:59:08 +0900

    bilstm, several tests

--- eval.sh
+++ eval.sh
@@ -12,4 +12,10 @@
 # python main.py --model gve --dataset iu --pretrained-model vgg16  --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/input_size8192&
 
 # 5.28 12:15  input8192, bilstm hid*4, sc ep20, gve ep30
-python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/input8192_bi_sc20&
+# python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/input8192_bi_sc20&
+
+# 5.28 13:15  bilstm hid*4, sc ep20, gve ep30
+# python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/bi_sc20&
+# python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/bi_sc10&
+# python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/bi_sc30&
+python main.py --model gve --dataset iu --pretrained-model vgg16  --num-epochs 30 --sc-ckpt ./data/iu/sentence_classifier_ckpt.pth > ./log_eval/bi_sc50&
--- pretrain.sh
+++ pretrain.sh
@@ -10,4 +10,10 @@
 # python main.py --model sc --dataset iu > ./log_pretrain/input_8192&
 
  # 5.28 11:50  input_8192, bilstm, epoch 20
-python main.py --model sc --dataset iu --num-epochs 20 > ./log_pretrain/input8192_bilstm_ep20&
+# python main.py --model sc --dataset iu --num-epochs 20 > ./log_pretrain/input8192_bilstm_ep20&
+
+ # 5.28 12:53   bilstm, epoch 20
+# python main.py --model sc --dataset iu --num-epochs 20 > ./log_pretrain/bilstm_ep20&
+# python main.py --model sc --dataset iu --num-epochs 10 > ./log_pretrain/bilstm_ep10&
+# python main.py --model sc --dataset iu --num-epochs 30 > ./log_pretrain/bilstm_ep30&
+python main.py --model sc --dataset iu --num-epochs 50 > ./log_pretrain/bilstm_ep50&
